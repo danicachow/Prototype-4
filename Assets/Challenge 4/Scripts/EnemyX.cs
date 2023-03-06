7a91403e651afbd2a48461ebfcbc2284b632df13
danicachow <121971462+danicachow@users.noreply.github.com>
2023-03-06 14:42:24 -0800

    changed enemy speed to private

--- Assets/Challenge 4/Scripts/EnemyX.cs
+++ Assets/Challenge 4/Scripts/EnemyX.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyX : MonoBehaviour
 {
-    public float speed = 75.0f;
+    private float speed = 75.0f;
     private Rigidbody enemyRb;
     private GameObject playerGoal;
 
